21e25cdf3774b24f5c10d9344d0a4c9c00275327
mAdkins <software@doorways.org>
2022-10-02 16:41:35 -0700

    Godoc comments for MP4 code

--- internal/mp4/time.go
+++ internal/mp4/time.go
@@ -8,6 +8,7 @@ import (
 	"github.com/abema/go-mp4"
 )
 
+// GetCreationTime acquires the creation time of an MP4 source.
 func GetCreationTime(source string) (time.Time, error) {
 	var ct time.Time
 
@@ -26,6 +27,10 @@ func GetCreationTime(source string) (time.Time, error) {
 	return ct, nil
 }
 
+// getMetadata returns the moov/mvhd section of the MP4 file.
+// This section contains various properties of the video.
+//
+// Spec resource: https://www.cimarronsystems.com/wp-content/uploads/2017/04/Elements-of-the-H.264-VideoAAC-Audio-MP4-Movie-v2_0.pdf
 func getMetadata(path string) ([]*mp4.BoxInfoWithPayload, error) {
 	if file, err := os.Open(path); err != nil {
 		return nil, fmt.Errorf("open file: %w", err)
